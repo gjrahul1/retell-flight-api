6e8c76c143dd2d2432bdbd040dc182ddc5106216
gjrahul1 <g.j.rahul1@gmail.com>
2025-08-24 12:46:15 +0530

    Changed from localhost to Prod

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "main:app", 
-        host="127.0.0.1",  # localhost instead of 0.0.0.0
+        host="0.0.0.0",  # localhost instead of 0.0.0.0
         port=port, 
         reload=settings.DEBUG
     )
